dc6042c15d87fe77d7f1c030d006431e2928f9ca
baphatle <baphatledev@gmail.com>
2024-09-06 23:19:13 +0700

    first commit

---
+++ src/components/MiddleBlock/styles.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const MiddleBlockSection = styled("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`;
+
+export const Content = styled("p")`
+  padding: 0.75rem 0 0.75rem;
+`;
+
+export const ContentWrapper = styled("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;
+
+export const TextInputWrapper = styled("div")`
+  margin-bottom: 1rem;
+
+  input {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+`;
